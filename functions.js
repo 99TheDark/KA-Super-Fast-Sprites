652e50d7ed3f13b830623b28a3a9c023dd96ab8d
99TheDark <104472362+99TheDark@users.noreply.github.com>
2023-01-14 22:37:34 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,40 +1,41 @@
-console.log("Sucessfully loaded!");
+(() => {
+    let programCode = function(processingInstance) {
+        with(processingInstance) {
+            size(400, 400);
+            noSmooth();
+        }
+    };
 
-var programCode = function(processingInstance) {
-    with(processingInstance) {
-        size(400, 400);
-        noSmooth();
-    }
-};
+    let canvas = document.createElement("canvas");
+    canvas.id = "thedark_superfastsprites";
+    document.body.appendChild(canvas);
 
-var canvas = document.createElement("canvas");
-canvas.id = "thedark_superfastsprites";
-document.body.appendChild(canvas);
+    let processingInstance = new Processing(canvas, programCode); 
 
-var processingInstance = new Processing(canvas, programCode); 
-
-var createSprite = function(data, scale) { 
-    // @OnlyTheDark
-    // https://www.khanacademy.org/computer-programming/super-fast-sprites/6098556280160256
-    background(255, 255, 255, 0);
-    noStroke();
-    var px = data.data;
-    var w = px[0].length;
-    var h = px.length;
-    scale = scale || floor(min(width / w, height / h));
-    for(var y = 0; y < h; y++) {
-        for(var x = 0; x < w; x++) {
-            var c = px[y][x];
-            if(c !== " ") {
-                fill(data[c]);
-                rect(x * scale, y * scale, scale + 0.5, scale + 0.5);
+    let createSprite = function(data, scale) { 
+        // @OnlyTheDark
+        // https://www.khanacademy.org/computer-programming/super-fast-sprites/6098556280160256
+        background(255, 255, 255, 0);
+        noStroke();
+        let px = data.data;
+        const w = px[0].length;
+        const h = px.length;
+        scale = scale || floor(min(width / w, height / h));
+        for(let y = 0; y < h; y++) {
+            for(let x = 0; x < w; x++) {
+                const c = px[y][x];
+                if(c !== " ") {
+                    fill(data[c]);
+                    rect(x * scale, y * scale, scale + 0.5, scale + 0.5);
+                }
             }
         }
-    }
-    var sprite = get(0, 0, w * scale, h * scale);
-    return sprite;
-};
+        let sprite = get(0, 0, w * scale, h * scale);
+        return sprite;
+    };
 
-window.createSprite = createSprite;
-
-console.log(window);
+    var exports = {};
+    exports.createSprite = createSprite;
+    
+    Object.defineProperty(exports, "__esModule", { "value": true });
+})();
