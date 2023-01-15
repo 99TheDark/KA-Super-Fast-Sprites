381d61b6bf77b4725f157346e76bb1b8ed7437fa
99TheDark <104472362+99TheDark@users.noreply.github.com>
2023-01-14 22:20:41 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,36 +1,40 @@
-console.log("Sucessfully loaded!");
+(() = {
+    console.log("Sucessfully loaded!");
 
-let programCode = function(processingInstance) {
-    with(processingInstance) {
-        size(400, 400);
-        noSmooth();
-    }
-};
+    let programCode = function(processingInstance) {
+        with(processingInstance) {
+            size(400, 400);
+            noSmooth();
+        }
+    };
 
-let canvas = document.createElement("canvas");
-canvas.id = "thedark_superfastsprites";
-document.body.appendChild(canvas);
+    let canvas = document.createElement("canvas");
+    canvas.id = "thedark_superfastsprites";
+    document.body.appendChild(canvas);
 
-let processingInstance = new Processing(canvas, programCode); 
+    let processingInstance = new Processing(canvas, programCode); 
 
-var createSprite = function(data, scale) { 
-    // @OnlyTheDark
-    // https://www.khanacademy.org/computer-programming/super-fast-sprites/6098556280160256
-    background(255, 255, 255, 0);
-    noStroke();
-    var px = data.data;
-    var w = px[0].length;
-    var h = px.length;
-    scale = scale || floor(min(width / w, height / h));
-    for(var y = 0; y < h; y++) {
-        for(var x = 0; x < w; x++) {
-            var c = px[y][x];
-            if(c !== " ") {
-                fill(data[c]);
-                rect(x * scale, y * scale, scale + 0.5, scale + 0.5);
+    var createSprite = function(data, scale) { 
+        // @OnlyTheDark
+        // https://www.khanacademy.org/computer-programming/super-fast-sprites/6098556280160256
+        background(255, 255, 255, 0);
+        noStroke();
+        var px = data.data;
+        var w = px[0].length;
+        var h = px.length;
+        scale = scale || floor(min(width / w, height / h));
+        for(var y = 0; y < h; y++) {
+            for(var x = 0; x < w; x++) {
+                var c = px[y][x];
+                if(c !== " ") {
+                    fill(data[c]);
+                    rect(x * scale, y * scale, scale + 0.5, scale + 0.5);
+                }
             }
         }
-    }
-    var sprite = get(0, 0, w * scale, h * scale);
-    return sprite;
-};
+        var sprite = get(0, 0, w * scale, h * scale);
+        return sprite;
+    };
+
+    // window.createSprite = createSprite;
+})();
