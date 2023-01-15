7f1af98ec4e68244dc00023f4bcf8914975184e2
99TheDark <104472362+99TheDark@users.noreply.github.com>
2023-01-14 22:16:46 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,36 @@
-var isImported = function() {
-    console.log("SUCESSFULLY IMPORTED");
-    return true;
+console.log("Sucessfully loaded!");
+
+let programCode = function(processingInstance) {
+    with(processingInstance) {
+        size(400, 400);
+        noSmooth();
+    }
+};
+
+let canvas = document.createElement("canvas");
+canvas.id = "thedark_superfastsprites";
+document.body.appendChild(canvas);
+
+let processingInstance = new Processing(canvas, programCode); 
+
+var createSprite = function(data, scale) { 
+    // @OnlyTheDark
+    // https://www.khanacademy.org/computer-programming/super-fast-sprites/6098556280160256
+    background(255, 255, 255, 0);
+    noStroke();
+    var px = data.data;
+    var w = px[0].length;
+    var h = px.length;
+    scale = scale || floor(min(width / w, height / h));
+    for(var y = 0; y < h; y++) {
+        for(var x = 0; x < w; x++) {
+            var c = px[y][x];
+            if(c !== " ") {
+                fill(data[c]);
+                rect(x * scale, y * scale, scale + 0.5, scale + 0.5);
+            }
+        }
+    }
+    var sprite = get(0, 0, w * scale, h * scale);
+    return sprite;
 };
